20e4085f6b7c5a2203a9a83e7af952ae6dafbe3c
Bakyalakshmi <116268707+bakya236@users.noreply.github.com>
2023-03-20 13:19:38 +0530

    modified the tables

--- my fashion studio.sql
+++ my fashion studio.sql
@@ -2,69 +2,70 @@ Create Database if not exists myfashionstudio;
 USE myfashionstudio;
 
 CREATE TABLE IF NOT EXISTS users(
-user_id int  PRIMARY KEY auto_increment,
-email_address VARCHAR (100),
-fullname VARCHAR(50),
+user_id int  PRIMARY KEY AUTO_INCREMENT,
+email_address VARCHAR (100) NOT NULL,
+fullname VARCHAR(50) NOT NULL,
 gender VARCHAR(25),
-mobile_number INT(50),
-password VARCHAR(50),
+mobile_number INT(50) NOT NULL,
+password VARCHAR(50) NOT NULL,
 dob DATE,
 hint_name VARCHAR(50),
 address VARCHAR(100)
 ); 
 
-alter table users
-modify email_address VARCHAR (100) not null;
-
-alter table users
-modify fullname VARCHAR(50) not null;
-
-INSERT INTO users (email_address,fullname,gender,mobile_number,password,dob,hint_name,address)
-VALUES 
-('gshdgh@gmail.com','bakya','female','9789853625','87duwy8','23-06-2023','bb','gfgdgs');
-
 select*
 from users;
-describe users;
 
 
--- CREATE TABLE IF NOT EXISTS category(
--- category_id VARCHAR (100) PRIMARY KEY,
--- category_name VARCHAR(50)
--- );
+-- INSERT INTO users (email_address,fullname,gender,mobile_number,password,dob,hint_name,address)
+-- VALUES 
+-- ('gshdgh@gmail.com','bakya','female','9789853625','87duwy8','23-06-2023','bb','gfgdgs');
 
--- CREATE TABLE IF NOT EXISTS products(
--- product_id VARCHAR (100) PRIMARY KEY,
--- product_name VARCHAR(50),
--- product_brand VARCHAR(25),
--- product_size INT(25),
--- product_color VARCHAR(50),
--- product_mrp INT(25),
--- product_offer INT(25)
--- );
+CREATE TABLE IF NOT EXISTS gender(
+gender_id INT (100) PRIMARY KEY AUTO_INCREMENT,
+gender_name VARCHAR(50) NOT NULL
+);
 
--- CREATE TABLE IF NOT EXISTS orders(
--- order_id VARCHAR (100) PRIMARY KEY,
--- order_size INT(25),
--- order_color  VARCHAR(25),
--- order_quantity INT(25),
--- order_date DATE
--- );
+INSERT INTO gender (gender_name)
+VALUES 
+('men'),('women'),('kids');
 
-describe orders;
+select*
+from gender;
 
-describe products;
+CREATE TABLE IF NOT EXISTS category(
+category_id INT (100) PRIMARY KEY AUTO_INCREMENT,
+category_name VARCHAR(50) NOT NULL,
+gender_id INT (100),
+FOREIGN KEY (gender_id) REFERENCES gender(gender_id)
+);
 
--- ALTER TABLE users
--- ALTER COLUMN
--- email_address VARCHAR (100) PRIMARY KEY,
--- fullname VARCHAR(50) NOT NULL,
--- gender VARCHAR(25),
--- mobile_number INT(50) NOT NULL,
--- password VARCHAR(50) NOT NULL,
--- dob DATE,
--- hint_name VARCHAR(50),
--- address VARCHAR(100);
+CREATE TABLE IF NOT EXISTS product(
+product_id INT (100) PRIMARY KEY AUTO_INCREMENT,
+product_name VARCHAR(50) NOT NULL,
+product_brand VARCHAR(25) NOT NULL,
+product_size INT(25) NOT NULL,
+product_color VARCHAR(50) NOT NULL,
+product_mrp INT(25) NOT NULL,
+product_offer INT(25)  NOT NULL,
+gender_id int(100),
+FOREIGN KEY (gender_id) REFERENCES gender(gender_id),
+category_id int(100),
+FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
+
+CREATE TABLE IF NOT EXISTS orders(
+order_id INT (100) PRIMARY KEY AUTO_INCREMENT,
+order_size INT(25),
+order_color  VARCHAR(25),
+order_quantity INT(25),
+order_date DATE,
+product_id int(100),
+FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+DESCRIBE orders;
+
 
 
 
